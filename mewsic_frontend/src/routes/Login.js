dbc1d3de53a32d0e8393426ca8c3a4c6663e0dc3
Hridhay-25 <104085757+Hridhay-25@users.noreply.github.com>
2023-11-22 15:13:01 +0530

    Update Login.js

--- mewsic_frontend/src/routes/Login.js
+++ mewsic_frontend/src/routes/Login.js
@@ -38,7 +38,7 @@ const LoginComponent = () => {
             <div className="inputRegion w-1/3 py-10 flex items-center justify-center flex-col">
                 {/*  I will have my 2 inputs(email and password) and I will have my sign up instead button*/}
                 <div className="font-bold mb-4">
-                    To continue, log in to Spotify.
+                    To continue, log in to Mewsic.
                 </div>
                 <TextInput
                     label="Email address or username"
@@ -69,7 +69,7 @@ const LoginComponent = () => {
                     Don't have an account?
                 </div>
                 <div className="border border-gray-500 text-gray-500 w-full flex items-center justify-center py-4 rounded-full font-bold">
-                    <Link to="/signup">SIGN UP FOR SPOTIFY</Link>
+                    <Link to="/signup">SIGN UP FOR MEWSIC</Link>
                 </div>
             </div>
         </div>
