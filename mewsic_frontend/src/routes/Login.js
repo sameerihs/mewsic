2fd2f1a7c6c492e39e0ee39805ac3c7ca9733f32
Zzyzzx2 <rahanshankar38@gmail.com>
2023-11-24 12:25:21 +0530

    UI improvements for login and signup page

--- mewsic_frontend/src/routes/Login.js
+++ mewsic_frontend/src/routes/Login.js
@@ -32,43 +32,47 @@ const LoginComponent = () => {
 
   return (
     <>
-      <div className="w-full h-full flex flex-col items-center">
-        <div className="logoDiv p-6">
-          <img src={mewsic_logo} alt="mewsic logo" width={125} />
-        </div>
-        <div className="inputRegion w-1/3 py-10 flex items-center justify-center flex-col">
-          {/*  I will have my 2 inputs(email and password) and I will have my sign up instead button*/}
-          <div className="font-bold mb-4">To continue, log in to Mewsic.</div>
-          <TextInput
-            label="Email address"
-            placeholder="Email address"
-            className="my-6"
-            value={email}
-            setValue={setEmail}
-          />
-          <PasswordInput
-            label="Password"
-            placeholder="Password"
-            value={password}
-            setValue={setPassword}
-          />
-          <div className=" w-full flex items-center justify-end my-8">
-            <button
-              className="bg-indigo-300 hover:bg-indigo-500 hover:text-gray-900 font-semibold p-3 px-10 rounded-full"
-              onClick={(e) => {
-                e.preventDefault();
-                login();
-              }}
-            >
-              LOG IN
-            </button>
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <div className="w-4/12 items-center bg-black opacity-80 p-4 rounded-xl">
+          <div className="pt-12 flex justify-center items-center">
+            <img src={mewsic_logo} alt="mewsic logo" width={125} />
           </div>
-          <div className="w-full border border-solid border-gray-300"></div>
-          <div className="my-6 font-semibold text-lg">
-            Don't have an account?
-          </div>
-          <div className="border border-gray-500 text-gray-500 hover:bg-black hover:text-white w-full flex items-center justify-center py-4 rounded-full font-bold">
-            <Link to="/signup">SIGN UP FOR MEWSIC</Link>
+          <div className="py-10 flex items-center justify-center flex-col">
+            {/*  I will have my 2 inputs(email and password) and I will have my sign up instead button*/}
+            <div className="font-bold mb-4 text-white">
+              To continue, log in to Mewsic.
+            </div>
+            <TextInput
+              label="Email address"
+              placeholder="Email address"
+              className="my-6"
+              value={email}
+              setValue={setEmail}
+            />
+            <PasswordInput
+              label="Password"
+              placeholder="Password"
+              value={password}
+              setValue={setPassword}
+            />
+            <div className=" w-full flex items-center justify-center my-8">
+              <button
+                className="bg-indigo-300 hover:bg-indigo-500 hover:text-gray-900 font-semibold p-3 px-10 rounded-full"
+                onClick={(e) => {
+                  e.preventDefault();
+                  login();
+                }}
+              >
+                LOG IN
+              </button>
+            </div>
+            <div className="w-full border border-solid border-gray-300 text-white"></div>
+            <div className="my-6 font-semibold text-lg text-white">
+              Don't have an account?
+            </div>
+            <div className="border border-gray-500 text-white hover:bg-indigo-400 hover:text-white w-full flex items-center justify-center py-4 rounded-full font-bold">
+              <Link to="/signup">SIGN UP FOR MEWSIC</Link>
+            </div>
           </div>
         </div>
       </div>
--- mewsic_frontend/src/routes/Signup.js
+++ mewsic_frontend/src/routes/Signup.js
@@ -6,6 +6,7 @@ import PasswordInput from "../components/shared/PasswordInput";
 import { Link, useNavigate } from "react-router-dom";
 import { makeUnauthenticatedPOSTRequest } from "../utils/serverHelpers";
 import mewsic_logo from "../assets/images/mewsic_logo.svg";
+import ParticlesBg from "particles-bg";
 
 const SignupComponent = () => {
   const [email, setEmail] = useState("");
@@ -40,77 +41,80 @@ const SignupComponent = () => {
   };
 
   return (
-    <div className="w-full h-full flex flex-col items-center">
-      <div className="logoDiv p-6">
-        <img src={mewsic_logo} alt="mewsic logo" width={125} />
-      </div>
-      <div className="inputRegion w-1/3 py-10 flex items-center justify-center flex-col">
-        {/*  I will have my 2 inputs(email and password) and I will have my sign up instead button*/}
-        <div className="font-bold mb-4 text-2xl">
-          Sign up for free to start listening.
+    <div className="w-full h-full flex flex-col items-center overflow-x-hidden">
+      <div className="w-4/12 items-center bg-black opacity-80 rounded-xl flex flex-col p-4 my-4">
+        <div className="logoDiv p-6 ">
+          <img src={mewsic_logo} alt="mewsic logo" width={125} />
         </div>
-        <TextInput
-          label="Email address"
-          placeholder="Enter your email"
-          className="my-6"
-          value={email}
-          setValue={setEmail}
-        />
-        <TextInput
-          label="Confirm Email Address"
-          placeholder="Enter your email again"
-          className="mb-6"
-          value={confirmEmail}
-          setValue={setConfirmEmail}
-        />
-        <TextInput
-          label="Username"
-          placeholder="Enter your username"
-          className="mb-6"
-          value={username}
-          setValue={setUsername}
-        />
-        <PasswordInput
-          label="Create Password"
-          placeholder="Enter a strong password here"
-          value={password}
-          setValue={setPassword}
-        />
-        <div className="w-full flex justify-between items-center space-x-8">
+        <div className="inputRegion flex items-center justify-center flex-col">
+          {/*  I will have my 2 inputs(email and password) and I will have my sign up instead button*/}
+          <div className="font-bold mb-4 text-2xl text-white">
+            Sign up for free to start listening.
+          </div>
           <TextInput
-            label="First Name"
-            placeholder="Enter Your First Name"
+            label="Email address"
+            placeholder="Enter your email"
             className="my-6"
-            value={firstName}
-            setValue={setFirstName}
+            value={email}
+            setValue={setEmail}
           />
           <TextInput
-            label="Last Name"
-            placeholder="Enter Your Last Name"
-            className="my-6"
-            value={lastName}
-            setValue={setLastName}
+            label="Confirm Email Address"
+            placeholder="Enter your email again"
+            className="mb-6"
+            value={confirmEmail}
+            setValue={setConfirmEmail}
           />
-        </div>
-        <div className=" w-full flex items-center justify-center     my-8">
-          <button
-            className="bg-indigo-300 font-semibold p-3 px-10 rounded-full"
-            onClick={(e) => {
-              e.preventDefault();
-              signUp();
-            }}
-          >
-            Sign Up
-          </button>
-        </div>
-        <div className="w-full border border-solid border-gray-300"></div>
-        <div className="my-6 font-semibold text-lg">
-          Already have an account?
-        </div>
-        <div className="border border-gray-500 text-gray-500 w-full flex items-center justify-center py-4 rounded-full font-bold">
-          <Link to="/login">LOG IN INSTEAD</Link>
+          <TextInput
+            label="Username"
+            placeholder="Enter your username"
+            className="mb-6"
+            value={username}
+            setValue={setUsername}
+          />
+          <PasswordInput
+            label="Create Password"
+            placeholder="Enter a strong password here"
+            value={password}
+            setValue={setPassword}
+          />
+          <div className="w-full flex justify-between items-center space-x-8">
+            <TextInput
+              label="First Name"
+              placeholder="First Name"
+              className="my-6"
+              value={firstName}
+              setValue={setFirstName}
+            />
+            <TextInput
+              label="Last Name"
+              placeholder="Last Name"
+              className="my-6"
+              value={lastName}
+              setValue={setLastName}
+            />
+          </div>
+          <div className=" w-full flex items-center justify-center     my-8">
+            <button
+              className="bg-indigo-300 hover:bg-indigo-500 hover:text-gray-900 font-semibold p-3 px-10 rounded-full"
+              onClick={(e) => {
+                e.preventDefault();
+                signUp();
+              }}
+            >
+              Sign Up
+            </button>
+          </div>
+          <div className="w-full border border-solid border-gray-300"></div>
+          <div className="my-6 font-semibold text-lg text-white">
+            Already have an account?
+          </div>
+          <div className="border border-gray-500 text-gray-500  hover:bg-indigo-400 hover:text-white w-full flex items-center justify-center py-4 rounded-full font-bold">
+            <Link to="/login">LOG IN INSTEAD</Link>
+          </div>
         </div>
       </div>
+      <ParticlesBg type="cobweb" bg={true} />
     </div>
   );
 };
